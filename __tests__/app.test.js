cc8074a063df82182668d8d6a7732bd011da1ef2
Jodi N Kansagor <joedvice@gmail.com>
2019-12-16 15:25:36 -0800

    post route test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,7 +6,11 @@ const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Trip = require('../lib/Models/Trip');
 
-
+// jest.mock('lib/services/weather.js', () => ({
+//   fetchWeather() {
+//     return Promise.resolve('My weather');
+//   }
+// }));
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -21,5 +25,19 @@ describe('app routes', () => {
     return mongoose.connection.close();
   });
 
-  it('creates a new trip')
+  it('creates a new trip', () => {
+    return request(app)
+      .post('/api/v1/trips')
+      .send({ name: 'new vacation', departureDate: 'December 22, 2019', returnDate: 'January 5, 2020', destination: 'New Jersey' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'new vacation',
+          departureDate: '2019-12-22T08:00:00.000Z',
+          returnDate: '2020-01-05T08:00:00.000Z',
+          destination: 'New Jersey',
+          __v: 0
+        });
+      });
+  });
 });
